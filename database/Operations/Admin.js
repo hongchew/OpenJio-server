81de41e1232449969e81bd3741c3cf71df4985cc
Sylvest Shu <shaoqiangsssq@gmail.com>
2020-09-27 20:09:49 +0800

    retrieve admin functions test

--- database/Operations/Admin.js
+++ database/Operations/Admin.js
@@ -2,6 +2,68 @@ const { Admin } = require('../Models/Admin');
 const ADMIN_TYPE = require('../../enum/AdminType');
 const { Sequelize } = require('sequelize');
 
+/*
+  Find and retrieve user from database with adminId
+  Parameters: (adminId: UUID)
+  Return: User object less password and salt (null if not found)
+*/
+const retrieveAdminByAdminId = async (adminId) => {
+  try {
+    const admin = await Admin.findOne({
+      where: {
+        adminId: adminId,
+      },
+      attributes: {
+        exclude: ['salt', 'password'],
+      },
+    });
+    return admin;
+  } catch (e) {
+    throw console.log(e);
+  }
+};
+
+/*
+  Retrieve admins by email from database
+  Parameters: (email: string)
+  Return: Admin object (null if not found)
+*/
+const retrieveAdminByEmail = async (email) => {
+  try {
+    const admin = await Admin.findOne({
+      where: {
+        email: email,
+      },
+    });
+    return admin;
+  } catch (e) {
+    throw console.log(e);
+  }
+};
+
+/*
+  Create and insert admin into database
+  Parameters: (email: string, password: string, name: string)
+  Return: User object of new user (null if not found)
+*/
+const createAdmin = async (name, email, password) => {
+  try {
+    const newAdmin = Admin.build({
+      name: name,
+      email: email,
+    });
+
+    newAdmin.salt = Admin.generateSalt();
+    newAdmin.password = Admin.encryptPassword(password, newAdmin.salt);
+
+    await newAdmin.save();
+
+    return await retrieveAdminByAdminId(newAdmin.adminId);
+  } catch (e) {
+    throw console.log(e);
+  }
+};
+
 const createAdmin = async (name, email, password) => {
   return createAdminGeneric(id, name, email, password, ADMIN_TYPE.ADMIN);
 };
@@ -26,15 +88,6 @@ const createAdminGeneric = async (name, email, password, type, adminId) => {
   await newAdmin.save();
 
   return newAdmin;
-};
-
-const retrieveAdminById = async (adminId) => {
-  const admin = await Admin.findOne({
-    where: {
-      adminId: adminId,
-    },
-  });
-  return admin;
 };
 
 module.exports = {
