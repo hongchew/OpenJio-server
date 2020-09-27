03b7a263ec79685a95502dec65c17c42a53e6e10
Sylvest Shu <shaoqiangsssq@gmail.com>
2020-09-28 02:15:03 +0800

    Added admin login functions and a test api

--- database/Operations/Admin.js
+++ database/Operations/Admin.js
@@ -1,6 +1,7 @@
 const { Admin } = require('../Models/Admin');
 const ADMIN_TYPE = require('../../enum/AdminType');
 const { Sequelize } = require('sequelize');
+const { nodemailer } = require('nodemailer');
 
 /*
   Find and retrieve user from database with adminId
@@ -19,7 +20,7 @@ const retrieveAdminByAdminId = async (adminId) => {
     });
     return admin;
   } catch (e) {
-    throw console.log(e);
+    throw console.error(e);
   }
 };
 
@@ -37,7 +38,27 @@ const retrieveAdminByEmail = async (email) => {
     });
     return admin;
   } catch (e) {
-    throw console.log(e);
+    throw console.error(e);
+  }
+};
+
+/*
+  Verify login credentials
+  Parameters: (email: string, password: string)
+  Return: Admin object if successful login, 
+*/
+const adminLogin = async (email, password) => {
+  try {
+    const admin = await retrieveAdminByEmail(email);
+    if (!admin || !admin.isCorrectPassword(password)) {
+      // no such email or wrong password
+      return null;
+    } else {
+      // email exist and password is correct
+      return await retrieveAdminByAdminId(Admin.adminId);
+    }
+  } catch (e) {
+    throw console.error(e);
   }
 };
 
@@ -46,32 +67,60 @@ const retrieveAdminByEmail = async (email) => {
   Parameters: (email: string, password: string, name: string)
   Return: User object of new user (null if not found)
 */
-const createAdmin = async (name, email, password) => {
+const createAdmin = async (username, name, email, password, adminType) => {
   try {
     const newAdmin = Admin.build({
       name: name,
       email: email,
+      username: username,
     });
 
     newAdmin.salt = Admin.generateSalt();
     newAdmin.password = Admin.encryptPassword(password, newAdmin.salt);
+    if (adminType === ADMIN_TYPE.SUPER_ADMIN) {
+      newAdmin.adminType = ADMIN_TYPE.SUPER_ADMIN;
+    } else {
+      newAdmin.adminType = ADMIN_TYPE.ADMIN;
+    }
 
     await newAdmin.save();
 
     return await retrieveAdminByAdminId(newAdmin.adminId);
   } catch (e) {
-    throw console.log(e);
+    throw console.error(e);
   }
 };
 
-const createAdmin = async (name, email, password) => {
-  return createAdminGeneric(id, name, email, password, ADMIN_TYPE.ADMIN);
+/*
+  Change password
+  Parameters: (Admin: object, newPassword: string)
+  Return: Admin object of existing user with new password (null if not found)
+*/
+const adminChangePassword = async (email, oldPassword, newPassword) => {
+  try {
+    const admin = await retrieveAdminByEmail(email);
+    //check if old and new password is the same
+    if (admin.isCorrectPassword(newPassword)) {
+      throw 'Password is the same';
+    } else {
+      admin.password = Admin.encryptPassword(newPassword, admin.salt);
+      await Admin.save();
+      return await retrieveAdminByAdminId(admin.adminId);
+    }
+  } catch (e) {
+    throw console.error(e);
+  }
 };
 
+/*
+  Function to initialise a super admin on running the server
+  Return: An admin object of super admin with id = 1
+*/
 const createSuperAdmin = async (id, name, email, password) => {
   return createAdminGeneric(name, email, password, ADMIN_TYPE.SUPER_ADMIN, id);
 };
 
+/*
 const createAdminGeneric = async (name, email, password, type, adminId) => {
   const newAdmin = Admin.build({
     adminId: adminId ? adminId : Sequelize.UUIDV4,
@@ -89,9 +138,13 @@ const createAdminGeneric = async (name, email, password, type, adminId) => {
 
   return newAdmin;
 };
+*/
 
 module.exports = {
   createAdmin,
   createSuperAdmin,
-  retrieveAdminById,
+  retrieveAdminByAdminId,
+  retrieveAdminByEmail,
+  adminLogin,
+  adminChangePassword
 };
---
+++ routes/adminRoutes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const {adminLogin} = require('../database/Operations/Admin');
+
+/*
+  Endpoint: POST /admin/login
+  Content type: JSON { email: 'string', password: 'string'}
+  Return: Models.Admin object 
+*/
+router.post('/adminLogin', async (req, res) => {
+    try {
+      const credentials = req.body;
+      const admin = await adminLogin(credentials.email, credentials.password);
+  
+      if (!admin) {
+        // login failed, either email or password wrong
+        res.status(401).json({message: 'Incorrect Email or Password'});
+      } else {
+        res.json(admin.name, admin.adminId);
+      }
+    } catch (e) {
+      res.status(500).json(e);
+    }
+  });
+  
+  module.exports = router;
