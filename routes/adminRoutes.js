db4dff488c176f4dc610b66936227795e6f18a46
ajax-yz <43975240+ajax-yz@users.noreply.github.com>
2020-10-01 01:55:26 +0800

    Changes made to update admin endpoint in adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -91,11 +91,14 @@ router.put('/reset-password', async (req, res) => {
 */
 router.put('/update-admin', async (req, res) => {
   try {
+    const adminId = req.body.adminId;
     const updatedAdmin = await updateAdmin(req.body);
     res.status(200).json(updatedAdmin);
   } catch (e) {
     //generic server error
-    res.status(500).json(e);
+    res.status(500).json({
+      message: 'Error updating Admin by ID: ' + adminId;
+    });
   }
 });
 
