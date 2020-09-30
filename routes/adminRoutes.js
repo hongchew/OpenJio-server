2a0fe655b3451a98c8ccc7c5c0c0a62c5f19cf93
Cheng Yang <e0203248@u.nus.edu>
2020-10-01 01:00:05 +0800

    adminRoutes done

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {createAdmin} = require('../database/Operations/Admin');
+const {createAdmin, changeAdminPassword, resetAdminPassword} = require('../database/Operations/Admin');
 const router = express.Router();
 
 /* http://localhost:3000/admin/ . */
@@ -29,25 +29,77 @@ router.post('/register', async (req, res) => {
     console.error(e);
     //delete sensitive information
     e.errors.forEach((err) => delete err.instance);
-
-    if (e.name === 'SequelizeValidationError') {
-      // Probably break null validation
-
-      res.status(400).json(e);
-    } else if (e.name === 'SequelizeUniqueConstraintError') {
-      // email is taken
-
-      // delete sensitive information
-      delete e.sql;
-      delete e.parent;
-      delete e.original;
-
-      res.status(400).json(e);
+    res.status(400).json(e);
     } else {
       // generic server error
       res.status(500).json(e);
     }
   }
 });
+
+/*
+  Endpoint: PUT /admin/change-user-password
+  Content type: JSON { email: 'string', currPassword: 'string', newPassword: 'string'}
+  Return: HTTP status code
+*/
+router.put('/change-password', async (req, res) => {
+  try {
+    const admin = await changeAdminPassword(
+      req.body.email,
+      req.body.currPassword,
+      req.body.newPassword
+    );
+
+    if (!admin) {
+      //current password is wrong
+
+      res.status(401).send();
+    } else {
+      res.status(200).send();
+    }
+  } catch (e) {
+    // generic server error
+    // res.status(500).json(e);
+    res.json(e);
+  }
+});
+
+/*
+  Endpoint: PUT /admin/reset-password
+  Content type: JSON { email: 'string'}
+  Return: HTTP status code
+*/
+router.put('/reset-password', async (req, res) => {
+  try {
+    await resetAdminPassword(req.body.email);
+    res.status(200).send();
+  } catch (e) {
+    // generic server error
+    res.status(500).json(e);
+  }
+});
+
+/*
+  Endpoint: PUT /admin/update-admin
+  Content type: JSON Model.User {
+    adminId: string,
+    userId: string,
+    name: string,
+    email: string,
+    adminType: string
+  } * only adminId is compulsory, every other field can be on a need-to-update basis.
+  Return: Model.User object with updated properties
+*/
+router.put('/update-admin', async (req, res) => {
+  try {
+    const updatedAdmin = await updateAdmin(req.body);
+    res.status(200).json(updatedAdmin);
+  } catch (e) {
+    //generic server error
+    res.status(500).json(e);
+  }
+});
+
+
 
 module.exports = router;
