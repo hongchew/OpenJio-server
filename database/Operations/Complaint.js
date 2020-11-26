ddafad9d1cb5cca829a867e8efc0bbf7a7d28e85
Cheng Yang <e0203248@u.nus.edu>
2020-11-27 01:39:26 +0800

    complaint API

--- database/Operations/Complaint.js
+++ database/Operations/Complaint.js
@@ -5,6 +5,7 @@ const axios = require('axios');
 const {retrieveAnnouncementByAnnouncementId} = require('./Announcement');
 const {strikeUser} = require('./User');
 const {sendNotification} = require('./Notifications');
+const User = require('../Models/User');
 
 /*
 Strike user associated with complaint
@@ -17,15 +18,15 @@ const strikeUserComplaint = async (userId, complaintId) => {
     if (!complaint) {
       throw `Complaint with ID ${complaintId} not found`;
     }
-    const request = retrieveRequestByRequestId(complaint.requestId);
+    const request = await retrieveRequestByRequestId(complaint.requestId);
     const requesterId = request.userId;
-    const announcement = retrieveAnnouncementByAnnouncementId(
+    const announcement = await retrieveAnnouncementByAnnouncementId(
       request.announcementId
     );
     const announcerId = announcement.userId;
     if (userId === requesterId) {
       //strike announcer
-      strikeUser(announcerId);
+      await strikeUser(announcerId);
       sendNotification(
         announcerId,
         'You have been striked',
@@ -33,7 +34,7 @@ const strikeUserComplaint = async (userId, complaintId) => {
       );
     } else {
       //strike requester
-      strikeUser(requesterId);
+      await strikeUser(requesterId);
       sendNotification(
         announcerId,
         'You have been striked',
@@ -104,6 +105,12 @@ const retrieveAllPendingComplaints = async () => {
       where: {
         complaintStatus: COMPLAINT_STATUS.PENDING,
       },
+      // include: {
+      //   model: User,
+      //   attributes: {
+      //     exclude: ['salt', 'password'],
+      //   },
+      // },
     });
     return complaints;
   } catch (e) {
